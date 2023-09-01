84bb12c9083404c883300e7e31e6204718c42a0f
Lucas De Sousa Santos <glucass@minsait.com>
2023-08-31 22:41:56 -0300

    iniciando os projetos do backend e frontend

---
+++ back/src/main/java/com/java/ccce/CcceApplication.java
@@ -0,0 +1,13 @@
+package com.java.ccce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcceApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/java/ccce/CcceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ccce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
