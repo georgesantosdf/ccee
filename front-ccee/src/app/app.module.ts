473db7d1a1720b3d995628188fe9ea4db22dbcf5
george.santos <lucas.fw@gmail.com>
2023-09-01 08:26:16 -0300

    Subindo o component do upload

--- front-ccee/src/app/app.module.ts
+++ front-ccee/src/app/app.module.ts
@@ -7,10 +7,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import {MatIconModule} from '@angular/material/icon'
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { uploadFormComponent } from './pages/upload-form/upload-form.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    uploadFormComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +34,18 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    HttpClientModule,
+    FormsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-ccee/src/app/pages/upload-form/upload-form.component.ts
@@ -0,0 +1,88 @@
+
+import { Component, ViewChild } from '@angular/core';
+import { AgenteService } from 'src/app/service/agente-service';
+import * as converter from 'xml-js';
+
+@Component({
+  selector: 'app-upload-form',
+  templateUrl: './upload-form.component.html',
+  styleUrls: ['./upload-form.component.css']
+})
+export class uploadFormComponent {
+
+  @ViewChild('arquivo') fileshtml:any;
+
+  porcentagemUpload = 0;
+  valueProgress = 0;
+  loading = 'none';
+
+
+  constructor(private service: AgenteService) {
+  }
+
+
+  public upload(event: any) {
+    this.loading = 'block';
+     for(let file of event.target.files) {
+       const reader: FileReader = new FileReader();
+       reader.onload = (ev: any) => {
+         let xml = ev.target.result;
+         let result1 = converter.xml2json(xml, { compact: true, spaces: 2 });
+         let JSONData:any = JSON.parse(result1);
+
+         let arr:any = [];
+
+         if(Array.isArray(JSONData.agentes.agente)){
+          arr = JSONData.agentes.agente;
+         }else{
+            arr.push(JSONData.agentes.agente);
+         }
+          this.criarListaDeagentes(arr);
+     }
+     reader.readAsText(file);
+   }
+ }
+
+ private criarListaDeagentes(obj: any) {
+
+    let i = 0;
+      for(let item of obj) {
+        let regioes = [];
+
+        for(let r of item.regiao) {
+          let reg  = {
+            sigla:r._attributes.sigla,
+            compra: { valor : this.ajustarLista(r.compra.valor)},
+            geracao:{ valor : this.ajustarLista(r.geracao.valor)}
+          };
+          regioes.push(reg);
+        }
+        let agente = {codigo : item.codigo._text, data : item.data._text, regioes: regioes};
+        this.progress(obj.size, i++)
+        this.post(agente);
+      }
+      alert("Processamento terminado");
+      this.valueProgress = 0;
+      this.loading = 'none';
+ }
+
+ private progress(val: number, i: number) {
+   return ((val * i) / 100) * 10;
+ }
+
+ private ajustarLista(valores:any){
+  let list = [];
+  for(let c of valores) {
+     list.push(c._text);
+  }
+  return list;
+ }
+
+ private post(agente:any) {
+  this.service.post(agente).subscribe({
+    next: (data) => {
+        }, error: (e) => alert("Error --> " + e.error)
+      });
+}
+
+}
