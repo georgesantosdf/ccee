05f15fb81239b5046b2e3f4b3348c7a730c02835
george.santos <lucas.fw@gmail.com>
2023-09-01 01:40:47 -0300

    Fazendo validação quando vier nulo do front entd

--- back/src/main/java/com/java/ccce/service/AgentService.java
+++ back/src/main/java/com/java/ccce/service/AgentService.java
@@ -3,11 +3,7 @@ package com.java.ccce.service;
 import com.java.ccce.entidade.Agent;
 import com.java.ccce.repository.AgentRepository;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
